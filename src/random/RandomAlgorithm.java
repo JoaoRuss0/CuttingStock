8a9be19dd1de3df2520339159073c352a2c44ab9
JoaoRuss0 <r0u6s12s18o@gmail.com>
2021-05-19 23:07:20 +0100

    First commit

---
+++ src/random/RandomAlgorithm.java
@@ -0,0 +1,41 @@
+package random;
+
+import algorithms.Algorithm;
+import algorithms.AlgorithmEvent;
+import algorithms.Individual;
+import algorithms.Problem;
+
+import java.util.Random;
+
+public class RandomAlgorithm<I extends Individual, P extends Problem<I>> extends Algorithm<I, P> {
+    //TODO this class might require the definition of additional methods and/or attributes
+
+    public RandomAlgorithm(int maxIterations, Random rand) {
+        super(maxIterations, rand);
+    }
+
+    @Override
+    public I run(P problem)
+    {
+        t = 0;
+        globalBest = problem.getNewIndividual();
+        globalBest.computeFitness();
+        fireIterationEnded(new AlgorithmEvent(this));
+
+        while (t < maxIterations && !stopped)
+        {
+            I new_individual = problem.getNewIndividual();
+            new_individual.computeFitness();
+
+            if(new_individual.getFitness() > globalBest.getFitness())
+            {
+                globalBest = new_individual;
+            }
+
+            t++;
+            fireIterationEnded(new AlgorithmEvent(this));
+        }
+        fireRunEnded(new AlgorithmEvent(this));
+        return globalBest;
+    }
+}
---
+++ src/stockingproblem/StockingProblemIndividual.java
@@ -0,0 +1,220 @@
+package stockingproblem;
+
+import algorithms.Algorithm;
+import algorithms.IntVectorIndividual;
+
+import java.util.*;
+
+public class StockingProblemIndividual extends IntVectorIndividual<StockingProblem, StockingProblemIndividual>
+{
+    private int[][] material;
+    private int num_cuts, materialMaxSize;
+
+    public StockingProblemIndividual(StockingProblem problem, int size)
+    {
+        super(problem, size);
+
+        // Fill genome with pieces from 0 to length - 1
+        for (int i = 0; i < genome.length; i++)
+        {
+            genome[i] = i;
+        }
+
+        shuffleGenome();
+        material = new int[problem.getMaterialHeight()][problem.getMaterialWidth()];
+    }
+
+    public StockingProblemIndividual(StockingProblemIndividual original)
+    {
+        super(original);
+        this.num_cuts = original.num_cuts;
+        this.materialMaxSize = original.materialMaxSize;
+        this.material = original.material.clone();
+    }
+
+    @Override
+    public double computeFitness()
+    {
+        ArrayList<Item> items = problem.getItems();
+        Item item_to_place;
+
+        int length = material[0].length;
+        materialMaxSize = 0;
+
+        // Fill material matrix
+        // Loop through genome and get items
+        for (int m = 0; m < genome.length; m++)
+        {
+            item_to_place = items.get(genome[m]);
+
+            // Loop material matrix from left to right
+            for (int i = 0; i < length; i++)
+            {
+                // Loop material matrix from top to bottom
+                for (int j = 0; j < material.length; j++)
+                {
+                    if(checkValidPlacement(item_to_place, material, j, i))
+                    {
+                        place_item_in_position(item_to_place, i, j);
+
+                        // Stop searching
+                        i = length;
+                        break;
+                    }
+                }
+            }
+        }
+
+        countCuts();
+
+        this.fitness = 0.7f * num_cuts + materialMaxSize * 0.3f;
+        return fitness;
+    }
+
+    private boolean checkValidPlacement(Item item, int[][] material, int lineIndex, int columnIndex) {
+        int[][] itemMatrix = item.getMatrix();
+        for (int i = 0; i < itemMatrix.length; i++) {
+            for (int j = 0; j < itemMatrix[i].length; j++) {
+                if (itemMatrix[i][j] != 0) {
+                    if ((lineIndex + i) >= material.length
+                            || (columnIndex + j) >= material[0].length
+                            || material[lineIndex + i][columnIndex + j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Fitness: ");
+        sb.append(fitness);
+        sb.append("\n");
+
+        sb.append("Genome: ");
+        sb.append(Arrays.toString(genome));
+        sb.append("\n");
+
+        sb.append("Material: \n");
+        for (int i = 0; i < problem.getMaterialHeight(); i++)
+        {
+            sb.append("[");
+            for (int j = 0; j < problem.getMaterialWidth(); j++)
+            {
+                if(material[i][j] == 0)
+                {
+                    sb.append("0 ");
+                    continue;
+                }
+                sb.append((char)material[i][j] + " ");
+            }
+            sb.append("]\n");
+        }
+        sb.append("\n");
+
+        sb.append("Number of cuts: ");
+        sb.append(num_cuts);
+        sb.append("\n");
+
+        sb.append("MaterialMaxSize: ");
+        sb.append(materialMaxSize);
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
+    /**
+     * @param i
+     * @return 1 if this object is BETTER than i, -1 if it is WORST than I and
+     * 0, otherwise.
+     */
+    @Override
+    public int compareTo(StockingProblemIndividual i) {
+        return (this.fitness == i.getFitness()) ? 0 : (this.fitness < i.getFitness()) ? 1 : -1;
+    }
+
+    @Override
+    public StockingProblemIndividual clone() {
+        return new StockingProblemIndividual(this);
+    }
+
+    private void shuffleGenome()
+    {
+        int stored_int, random_pos, length = genome.length;
+
+        // Loop through genome
+        for (int i = 0; i < length; i++)
+        {
+            // Get a random position, between 0 <-> length-1
+            random_pos = Algorithm.random.nextInt(length);
+
+            // Switch values from i position to random position
+            stored_int = genome[i];
+            genome[i] = genome[random_pos];
+            genome[random_pos] = stored_int;
+        }
+    }
+
+    private void place_item_in_position(Item item_to_place, int i, int j)
+    {
+        int[][] item_matrix = item_to_place.getMatrix();
+        int right_most_column = j + item_to_place.getColumns();
+
+        if(materialMaxSize < right_most_column)
+        {
+            materialMaxSize = right_most_column;
+        }
+
+        // Loop item lines
+        for (int k = 0; k < item_matrix.length; k++)
+        {
+            // Loop item columns
+            for (int l = 0; l < item_matrix[0].length; l++)
+                {
+                // Only place if value inside item position is 1
+                if(item_matrix[k][l] != 0)
+                {
+                    material[j+k][i+l] = item_to_place.getRepresentation();
+                }
+            }
+        }
+    }
+
+    private void countCuts()
+    {
+        num_cuts = 0;
+
+        // Count horizontal cuts
+        // Loop material matrix from top to bottom
+        for (int i = 0; i < material.length; i++)
+        {
+            // Loop material matrix from left to right
+            for (int j = 0; j < material[0].length - 1; j++)
+            {
+                if(material[i][j] != material[i][j + 1])
+                {
+                    num_cuts++;
+                }
+            }
+        }
+
+        // Count vertical cuts
+        // Loop material matrix from left to right
+        for (int i = 0; i < material[0].length; i++)
+        {
+            // Loop material matrix from top to bottom
+            for (int j = 0; j < material.length - 1; j++)
+            {
+                if(material[j][i] != material[j + 1][i])
+                {
+                    num_cuts++;
+                }
+            }
+        }
+
+        num_cuts+= problem.getMaterialHeight();
+    }
+}
